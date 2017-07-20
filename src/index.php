66ff594526c67db8c2e78ed0f6b3f5bfd9a0f254
Fumitaka Tokumitsu <toku345@gmail.com>
2017-07-20 10:52:24 +0900

    Update index.php

--- src/index.php
+++ src/index.php
@@ -26,6 +26,7 @@
                 ?>
 
             </div>
+            <div>Yeahhhhhhhhhhhhhhhhhhhhhhh!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</div>
         </div>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
